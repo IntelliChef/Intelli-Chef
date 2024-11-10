53f55748102167684b86eb5637995bff1d9036fe
Fatir Yousaf <fatiryousaf@gmail.com>
2024-11-10 15:01:07 -0500

    Made Ingredient class in entities

---
+++ src/main/java/com/intelliChef/entities/Ingredient.java
@@ -0,0 +1,11 @@
+package com.intelliChef.entities;
+
+public class Ingredient {
+    private final String name;
+    private final int quantity;
+
+    public Ingredient(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
