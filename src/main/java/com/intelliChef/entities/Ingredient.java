48b884fa58989cd5ff91f2ab1413296afb54a8e6
Fatir Yousaf <fatiryousaf@gmail.com>
2024-11-10 15:05:53 -0500

    Added getters to Ingredient

--- src/main/java/com/intelliChef/entities/Ingredient.java
+++ src/main/java/com/intelliChef/entities/Ingredient.java
@@ -4,6 +4,14 @@ public class Ingredient {
     private final String name;
     private final int quantity;
 
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public Ingredient(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
