ec1dd1fab1c51410978f959d5e0ad1c0fbd6b0e7
Fatir Yousaf <fatiryousaf@gmail.com>
2024-11-22 16:25:26 -0500

    Made a new view to confirm the ingredient or try again

---
+++ src/main/java/com/intelliChef/view/IngredientsDetectedView.java
@@ -0,0 +1,45 @@
+package com.intelliChef.view;
+
+import com.intelliChef.entities.Ingredient;
+import com.intelliChef.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class IngredientsDetectedView extends JFrame {
+     public IngredientsDetectedView(List<Ingredient> ingredientList) {
+         super("Ingredients Detected");
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         setSize(400, 200);
+         setLocationRelativeTo(null);
+         setLayout(new BorderLayout());
+
+         JLabel ingredientCount = new JLabel("Number of ingredients detected: "
+                 + ingredientList.size(), SwingConstants.CENTER);
+         ingredientCount.setFont(new Font("SansSerif", Font.PLAIN, 16));
+         add(ingredientCount, BorderLayout.CENTER);
+
+         JButton continueButton = new JButton("Continue");
+         continueButton.addActionListener(e -> {
+             Main.showIngredientListView(ingredientList);
+             dispose();
+         });
+
+         JButton cancelButton = new JButton("Cancel");
+         cancelButton.addActionListener(e -> {
+             Main.showRecipeUploadView();
+             dispose();
+         });
+
+         JPanel buttonPanel = new JPanel();
+         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+         buttonPanel.add(continueButton);
+         buttonPanel.add(cancelButton);
+         add(buttonPanel, BorderLayout.SOUTH);
+
+         setVisible(true);
+     }
+
+
+}
