c9d66a8202863028a90f68c0d418d0e20e65ddce
Fatir Yousaf <fatiryousaf@gmail.com>
2024-11-23 20:56:32 -0500

    Added factory(still need to determine its use) and a view manager to switch between views

--- src/main/java/com/intelliChef/use_case/InteractorFactory.java
+++ src/main/java/com/intelliChef/use_case/InteractorFactory.java
@@ -2,15 +2,11 @@ package com.intelliChef.use_case;
 
 import com.intelliChef.data_access.GeminiAIClient;
 import com.intelliChef.use_case.analyzeImage.AnalyzeImageInteractor;
-import com.intelliChef.use_case.ingredientDetected.IngredientsDetectedInteractor;
 
 public class InteractorFactory {
     public Object create(String interactorName, GeminiAIClient geminiAIClient) {
         if (interactorName.equals("AnalyzeImage")) {
             return new AnalyzeImageInteractor(geminiAIClient);
-        }
-        else if (interactorName.equals("IngredientsDetected")) {
-            return new IngredientsDetectedInteractor();
         }
         return null;
     }
