ebd616f7023eb4ac728da1ba938ecb4b520a8bbf
Fatir Yousaf <fatiryousaf@gmail.com>
2024-11-23 19:28:39 -0500

    Forgot to add the interactor factory to stage

--- src/main/java/com/intelliChef/use_case/InteractorFactory.java
+++ src/main/java/com/intelliChef/use_case/InteractorFactory.java
@@ -1,4 +1,18 @@
 package com.intelliChef.use_case;
 
+import com.intelliChef.data_access.GeminiAIClient;
+import com.intelliChef.use_case.analyzeImage.AnalyzeImageInteractor;
+import com.intelliChef.use_case.ingredientDetected.IngredientsDetectedInteractor;
+import com.intelliChef.view.IngredientsDetectedView;
+
 public class InteractorFactory {
+    public Object create(String interactorName, GeminiAIClient geminiAIClient) {
+        if (interactorName.equals("AnalyzeImage")) {
+            return new AnalyzeImageInteractor(geminiAIClient);
+        }
+        else if (interactorName.equals("IngredientsDetected")) {
+            return new IngredientsDetectedInteractor();
+        }
+        return null;
+    }
 }
