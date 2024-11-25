755915124f5820eb381c8c7a63de04928fc88af2
Fatir Yousaf <fatiryousaf@gmail.com>
2024-11-25 01:48:41 -0500

    Implemented the methods for ingredient repo

--- src/main/java/com/intelliChef/data_access/IngredientListRepository.java
+++ src/main/java/com/intelliChef/data_access/IngredientListRepository.java
@@ -3,10 +3,11 @@ package com.intelliChef.data_access;
 import com.intelliChef.entities.Ingredient;
 import com.intelliChef.use_case.IngredientRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class IngredientListRepository implements IngredientRepository {
-    private List<Ingredient> myIngredients;
+    private final List<Ingredient> myIngredients = new ArrayList<Ingredient>();
 
     @Override
     public List<Ingredient> getAllIngredients() {
@@ -15,7 +16,22 @@ public class IngredientListRepository implements IngredientRepository {
 
     @Override
     public void addIngredient(Ingredient ingredient) {
-
+        myIngredients.add(ingredient);
     }
-    // to be implemented later.
+
+    @Override
+    public void deleteIngredient(int id) {
+        for (Ingredient ingredient : myIngredients) {
+            if (ingredient.getId() == (id)) {
+                myIngredients.remove(ingredient);
+                break;
+            }
+        }
+        System.out.println("Ingredient ID doesn't exist");
+    }
+
+    @Override
+    public int getNextId() {
+        return myIngredients.size() + 1; // Might have to change this
+    }
 }
